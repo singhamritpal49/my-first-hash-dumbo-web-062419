8cf0fd2b0fff04af46cbcbefb9f2a9c7f77983e6
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-14 22:55:07 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -41,6 +41,10 @@ def adding
 shipping_manifest["muskets"]=2
 binding.pry
 
+shipping_manifest.each do |key,value|
+binding.pry
+end
+
   # add 4 gun powder to the shipping_manifest hash below
 shipping_manifest["gun powder"]=4
 
