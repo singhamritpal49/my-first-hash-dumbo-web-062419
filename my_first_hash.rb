b63ec94973492afaee21302d37bcf7338bf2f09c
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-14 22:54:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,6 +36,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+                
   # add 2 muskets to the shipping_manifest hash below
 shipping_manifest["muskets"]=2
 binding.pry
@@ -47,3 +48,5 @@ shipping_manifest["gun powder"]=4
   # return the shipping_manifest hash below
 shipping_manifest
 end
+
+
